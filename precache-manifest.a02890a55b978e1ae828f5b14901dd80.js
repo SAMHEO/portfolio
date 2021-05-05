132123586345bcc7ffb2de472b889676d92c8743
Samnyeong Heo <hsn1017@gmail.com>
2021-05-04 22:54:55 -0400

    Updates

---
+++ precache-manifest.a02890a55b978e1ae828f5b14901dd80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35227aa53f713c69308f4dfb3059cd90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21b28e7252d94af408ba",
+    "url": "/static/css/2.fc88e6b0.chunk.css"
+  },
+  {
+    "revision": "87de0f3212aadf95ba90",
+    "url": "/static/css/main.bce11bec.chunk.css"
+  },
+  {
+    "revision": "21b28e7252d94af408ba",
+    "url": "/static/js/2.a328e732.chunk.js"
+  },
+  {
+    "revision": "87de0f3212aadf95ba90",
+    "url": "/static/js/main.752131f8.chunk.js"
+  },
+  {
+    "revision": "0c1400ec95fcc8fa04a7",
+    "url": "/static/js/runtime~main.5134c318.js"
+  }
+]);
